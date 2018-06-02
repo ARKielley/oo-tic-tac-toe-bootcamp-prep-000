00f1c41dcd42306fcc3603e5c23ca5934ac31c33
Amanda Kielley <eatingfoil@gmail.com>
2018-06-02 06:20:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,11 +2,13 @@ class TicTacToe
   def initialize
     @board = Array.new(9, " ")
   end
+  
   WIN_COMBINATIONS = [
   [0,1,2], [3,4,5], [6,7,8],
   [0,3,6], [1,4,7], [2,5,8],
   [0,4,8], [2,4,6]
   ]
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -14,10 +16,16 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
   def input_to_index(input)
     input.to_i - 1 
   end
+  
   def move(i, token)
     @board[i] = token 
+  end
+  
+  def position_taken?(i)
+    !(@board[i].nil? || @board[i] == " " || @board[i] == "")
   end
 end
